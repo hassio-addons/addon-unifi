73fab4b84dde4618abf24427d00e4936e4bee28d
Franck Nijhof <frenck@addons.community>
2019-04-17 18:27:55 +0200

    :hammer: Simplifies add-on code logic

--- unifi/rootfs/etc/cont-init.d/10-requirements.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Community Hass.io Add-ons: UniFi Controller
-# This files check if all user configuration requirements are met
-# ==============================================================================
-bashio::config.require.ssl
--- unifi/rootfs/etc/cont-init.d/unifi.sh
+++ unifi/rootfs/etc/cont-init.d/unifi.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Community Hass.io Add-ons: UniFi Controller
-# Ensures the data of the UniFi controller is store outside the container
+# Configures the UniFi Controller
 # ==============================================================================
 readonly properties="/data/unifi/data/system.properties"
 
+bashio::config.require.ssl
+
+# Ensures the data of the UniFi controller is store outside the container
+if ! bashio::fs.directory_exists '/data/unifi/data'; then
+    mkdir -p /data/unifi/data
+fi
+rm -fr /usr/lib/unifi/data
+ln -s /data/unifi/data /usr/lib/unifi/data
+
+if ! bashio::fs.directory_exists '/backup/unifi'; then
+    mkdir -p /backup/unifi
+fi
+rm -fr /usr/lib/unifi/data/backup
+ln -s /backup/unifi /usr/lib/unifi/data/backup
+
+# Enable small files on MongoDB
 if ! bashio::fs.file_exists "${properties}"; then
     touch "${properties}"
     echo "unifi.db.extraargs=--smallfiles" > "${properties}"
